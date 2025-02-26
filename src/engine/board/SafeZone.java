d3ee8413c796f82462876a7c7046a75fa9cdd43b
Seif Alaa <seif.alaa1231@gmail.com>
2025-02-26 12:19:15 +0200

    Built SafeZone class

---
+++ src/engine/board/SafeZone.java
@@ -0,0 +1,32 @@
+package engine.board;
+
+import java.util.ArrayList;
+
+
+public class SafeZone {
+	// READ ONLY attributes
+	private final Colour colour;
+	private final ArrayList<Cell> cells;
+
+	// Constructor that initializes colour of safe zone and add 4 safe cells to the
+	// safe zone
+	
+	public SafeZone(Colour colour) {
+		this.colour = colour;
+		cells = new ArrayList<Cell>();
+		for (int i = 0; i < 4; i++) {
+			cells.add(new Cell(CellType.SAFE));
+		}
+	}
+	
+
+	// Getters for safe zone attributes
+	public Colour getColour() {
+		return colour;
+	}
+
+	public ArrayList<Cell> getCells() {
+		return cells;
+	}
+
+}
