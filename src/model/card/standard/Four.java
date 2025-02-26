9731c86b215eb1d4e392f9ea0d6d8fa9d207f94a
Seif Alaa <seif.alaa1231@gmail.com>
2025-02-26 12:52:58 +0200

    Built Saver class

--- src/model/card/standard/Four.java
+++ src/model/card/standard/Four.java
@@ -1,5 +1,8 @@
 package model.card.standard;
 
-public class Four {
-
+public class Four extends Standard{
+	Four(String name, String description, Suit suit, BoardManager boardManager, GameManager
+			gameManager){
+		super(name, description, 4, suit, boardManager, gameManager);//initialize using super constructer, with rank as 4
+	}
 }
---
+++ src/model/card/wild/Saver.java
@@ -0,0 +1,7 @@
+package model.card.wild;
+
+public class Saver extends Wild {
+	Saver(String name, String description, BoardManager boardManager, GameManager gameManager) {
+		super(name, description, boardManager, gameManager);
+	}
+}
