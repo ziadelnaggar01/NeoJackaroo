f17a6c793789524f188c0818b48391c2dfbea1c5
Ziad Elnaggar <ziad.elnaggar@gmail.com>
2025-02-28 21:28:24 +0200

    Restore essential code in Game class
    In the latest commit, an essential part of the Game class was accidentally removed. This commit restores the missing functionality to ensure the game logic works as expected.

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -12,5 +12,32 @@ import model.player.CPU;
 import model.player.Player;
 
 public class Game implements GameManager {
-	
+	private final Board board;
+	private final ArrayList<Player> players;
+	private final ArrayList<Card> firePit;
+	private int currentPlayerIndex;
+	private int turn;
+    
+	public Game(String playerName) throws IOException {
+		ArrayList<Colour> colours = new ArrayList<>();
+		colours.add(Colour.RED);
+		colours.add(Colour.GREEN);
+		colours.add(Colour.BLUE);
+		colours.add(Colour.YELLOW);
+
+		Collections.shuffle(colours);// built in method to shuffle elements in a list, can use a seed with it
+		this.board = new Board(colours, this);
+		Deck.loadCardPool(this, board);
+		players.add(new Player(playerName, colours.get(0)));
+		players.add(new CPU("CPU 1", colours.get(1), board));
+		players.add(new CPU("CPU 2", colours.get(2), board));
+		players.add(new CPU("CPU 3", colours.get(3), board));
+		for (int i=0;i<4;i++) {
+			players.get(i).setHand(Deck.drawCards());
+		}
+		this.currentPlayerIndex = 0;
+		this.turn = 0;
+		this.firePit = new ArrayList<>();
+
+	}
 }
