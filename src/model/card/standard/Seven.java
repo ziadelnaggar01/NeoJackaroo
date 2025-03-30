e000fed9ef48898068fdcd5fa6e0169b6f97cc05
Ziad Elnaggar <ziad.elnaggar@gmail.com>
2025-03-30 14:27:18 +0200

    Overrode Seven card logic to implement unique behavior
    Overrode validateMarbleSize to ensure the Seven card action can be executed with up to two marbles.
    
    Overrode validateMarbleColours to validate that selected marbles belong to the active player.
    
    Overrode act method to define the Seven card’s distinct actions:
    
    If one marble is selected, it can move a total of 7 steps.
    
    If two marbles are selected, the 7 steps can be split between them in any valid combination.

--- src/model/card/standard/Seven.java
+++ src/model/card/standard/Seven.java
@@ -1,8 +1,14 @@
 package model.card.standard;
 
+import java.util.ArrayList;
+
 import engine.GameManager;
 
 import engine.board.BoardManager;
+import exception.ActionException;
+import exception.InvalidMarbleException;
+import model.Colour;
+import model.player.Marble;
 
 /** Represents a Seven card, a subclass of Standard with a rank of 7. */
 
@@ -20,5 +26,69 @@ public class Seven extends Standard {
 
 	public Seven(String name, String description, Suit suit, BoardManager boardManager, GameManager gameManager) {
 		super(name, description, 7, suit, boardManager, gameManager);
+	}
+	
+	/**
+     * Validates whether the given list of marbles contains 1 or 2 marbles.
+     * 
+     * @param marbleList The list of marbles to validate.
+     * @return true if the list contains one or two marbles, false otherwise.
+     */
+	@Override
+	public boolean validateMarbleSize(ArrayList<Marble> marbles)
+	{
+		int numberOfMarbles = marbles.size();
+		return numberOfMarbles==1 || numberOfMarbles==2;
+	}
+	
+	/**
+	 * Validates the selected marbles all belong to active player.
+	 *
+	 * @param marbles The list of marbles to check.
+	 * @return true if the selection follows the rules, false otherwise.
+	 */
+	@Override
+	public boolean validateMarbleColours(ArrayList<Marble> marbles)
+	{
+		Colour activePlayerColour = gameManager.getActivePlayerColour();
+		int numberOfMarbles = marbles.size();
+		for(int i=0; i<numberOfMarbles; i++)
+			if(!marbles.get(i).getColour().equals(activePlayerColour)) 
+				return false;
+		return true;
+	}
+	
+	/**
+	 * Executes the Seven card action:
+	 * - If one marble is selected, it moves forward by 7 steps.
+	 * - If two marbles are selected, moves both marbles a combined total of 7 steps.
+	 *   The first marble moves `getSplitDistance()`, and the second moves `7 - getSplitDistance()`.
+	 *
+	 * @param  marbles The list of selected marbles (should be 1 or 2)
+	 * @throws InvalidMarbleException If more than one marble is selected or if the marble does not belong to the active player.
+	 * @throws ActionException If the action cannot be executed due to game constraints.
+	 */
+	@Override
+	public void act(ArrayList<Marble> marbles) throws ActionException , InvalidMarbleException
+	{
+		if(!validateMarbleSize(marbles))
+			throw new InvalidMarbleException("Seven card allows selecting only one or two marbles.");
+		if(!validateMarbleColours(marbles))
+			throw new InvalidMarbleException("Selected marbles must belong to the active player.");
+		
+		int numberOfMarbles = marbles.size();
+		switch(numberOfMarbles)
+		{
+		case 1:
+			Marble selectedMarble = marbles.get(0);
+			boardManager.moveBy(selectedMarble, 7, false);
+			break;
+		case 2:
+			Marble marble1 = marbles.get(0);
+			Marble marble2 = marbles.get(1);
+			boardManager.moveBy(marble1, boardManager.getSplitDistance(), false);
+			boardManager.moveBy(marble2, 7-boardManager.getSplitDistance(), false);
+			break;
+		}
 	}
 }
