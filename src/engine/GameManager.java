a16c696dadf2dbb537865948033c9df8d064b745
Ziad Elnaggar <ziad.elnaggar@gmail.com>
2025-03-31 19:28:29 +0200

    Merge updates in game interfaces with main

--- src/engine/GameManager.java
+++ src/engine/GameManager.java
@@ -1,10 +1,23 @@
 package engine;
 
+import model.*;
+import model.player.Marble;
+import exception.*;
 /**
  * An interface that will allow other classes down the hierarchy to communicate
  * with the Game class.
  */
 
 public interface GameManager {
+	void sendHome(Marble marble);
 
+	void fieldMarble() throws CannotFieldException, IllegalDestroyException;
+
+	void discardCard(Colour colour) throws CannotDiscardException;
+
+	void discardCard() throws CannotDiscardException;
+
+	Colour getActivePlayerColour();
+
+	Colour getNextPlayerColour();
 }
