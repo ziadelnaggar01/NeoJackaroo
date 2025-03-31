f8b95cfa3c39db17cc53b6937851abaaa85258d6
Seif Alaa <seif.alaa1231@gmail.com>
2025-03-31 16:55:31 +0200

    Add methods to BoardManager interface

--- src/engine/board/BoardManager.java
+++ src/engine/board/BoardManager.java
@@ -1,9 +1,27 @@
 package engine.board;
 
+import model.player.Marble;
+
+import java.util.ArrayList;
+
+import exception.*;
+
 /**
  * An interface that will allow other classes down the hierarchy to communicate
  * with the Board class.
  */
 public interface BoardManager {
 	int getSplitDistance();
+
+	void moveBy(Marble marble, int steps, boolean destroy) throws IllegalMovementException, IllegalDestroyException;
+
+	void swap(Marble marble, Marble marble2) throws IllegalSwapException;
+
+	void destroyMarble(Marble marble) throws IllegalDestroyException;
+
+	void sendToBase(Marble marble) throws CannotFieldException, IllegalDestroyException;
+
+	void sendToSafe(Marble marble) throws InvalidMarbleException;
+
+	ArrayList<Marble> getActionableMarbles();
 }
