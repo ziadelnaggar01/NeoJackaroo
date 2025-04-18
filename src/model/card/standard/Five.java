82881c471124f027970af5a9992b4821c32e29ab
Ziad Elnaggar <ziad.elnaggar@gmail.com>
2025-04-18 19:39:01 +0200

    Refactor: Remove redundant overrides of validateMarbleSize and validateMarbleColor in Ace, Queen, and Saver
    Removed duplicate method overrides of validateMarbleSize and validateMarbleColor from Ace, Queen, and Saver classes. Although the logic was identical to the base implementation, these overrides caused conflicts with unit tests and introduced unnecessary lines of code. This cleanup improves maintainability and eliminates redundancy.

--- src/model/card/standard/Five.java
+++ src/model/card/standard/Five.java
@@ -6,6 +6,7 @@ import engine.GameManager;
 import engine.board.BoardManager;
 import exception.ActionException;
 import exception.InvalidMarbleException;
+import model.Colour;
 import model.player.Marble;
 
 /** Represents a Five card, a subclass of Standard with a rank of 5. */
@@ -23,6 +24,16 @@ public class Five extends Standard {
 
 	public Five(String name, String description, Suit suit, BoardManager boardManager, GameManager gameManager) {
 		super(name, description, 5, suit, boardManager, gameManager);
+	}
+	
+	/**
+	 * Validates whether the given list of marbles contains the correct color of marbles that each card is supposed to act on (same color as the player or not based on the type of card).
+	 * @param marbles
+	 * @return true if color of marble matches card action, false otherwise.
+	 */
+	public boolean validateMarbleColours(ArrayList<Marble> marbles)
+	{
+		return true;
 	}
 	
 	 /**
